0ab49616a015ec9d2291e4363eef2c48b3c36eb7
Emmanuel Egito Oliveira Candeira Marinho <e_egito@hotmail.com>
2024-01-16 13:14:55 -0300

    Criacao da estrutura do backend

---
+++ src/backend/TaskControlApp/TaskControlApp.API/Controllers/TaskController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskControlApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+    }
+}
